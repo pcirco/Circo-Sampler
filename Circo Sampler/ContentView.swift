d7110fb25933c8c48b0ed81fe91f1fde74babc78
Paul Circo <paul.circo@gmail.com>
2021-04-15 13:46:24 -0500

    day 1

--- Circo Sampler/ContentView.swift
+++ Circo Sampler/ContentView.swift
@@ -6,12 +6,47 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    
+    @State var recorder: AVAudioRecorder?
+    @State var player: AVAudioPlayer?
+    
+    @State var docDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    @State var audioFile = URL(string: "")
+    
     var body: some View {
-        Text("test")
-            .padding()
+        HStack{
+            Text("to be continued...")
+        }
     }
+    
+    func setup() {
+        
+        audioFile = docDir
+        audioFile?.appendPathComponent("scratch.caf")
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(.playAndRecord)
+        } catch {
+            print(error)
+        }
+        
+        let recordingSettings = [AVFormatIDKey: kAudioFormatMPEG4AAC, AVSampleRateKey: 44100.0, AVNumberOfChannelsKey: 2, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue] as [String: Any]
+        
+        do {
+            try recorder = AVAudioRecorder(url: audioFile!, settings: recordingSettings)
+        } catch {
+            print("recorder setup issues = \(error)")
+        }
+        
+        recorder?.prepareToRecord()
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
