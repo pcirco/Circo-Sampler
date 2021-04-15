b42a471d7f8a2c2b4b83e21d4a38271e25a56ebd
Paul Circo <paul.circo@gmail.com>
2021-04-15 12:52:06 -0500

    initial commit

---
+++ Circo Sampler/Circo_SamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circo_SamplerApp.swift
+//  Circo Sampler
+//
+//  Created by Paul Circo on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Circo_SamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circo Sampler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Circo Sampler
+//
+//  Created by Paul Circo on 4/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("test")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
