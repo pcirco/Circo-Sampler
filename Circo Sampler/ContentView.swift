1684af9d01a0c3bf959f55e522d0e9d36a879b24
Paul Circo <paul.circo@gmail.com>
2021-04-27 13:48:32 -0500

    more functionality added

--- Circo Sampler/ContentView.swift
+++ Circo Sampler/ContentView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 import AVKit
 
 struct ContentView: View {
+
+    var body: some View {
+        TestView()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct TestView: View {
     
     @State var recorder: AVAudioRecorder?
     @State var masterPlayer: AVAudioPlayer?
     
     @State var player1: AVAudioPlayer?
     @State var player2: AVAudioPlayer?
+    @State var player3: AVAudioPlayer?
+    @State var player4: AVAudioPlayer?
+    @State var player5: AVAudioPlayer?
+    @State var player6: AVAudioPlayer?
+    @State var player7: AVAudioPlayer?
+    @State var player8: AVAudioPlayer?
     
     @State var docDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
     @State var audioFile = URL(string: "")
@@ -52,7 +72,7 @@ struct ContentView: View {
             setup()
         }
         
-        Slider(value: $rate, in: 0.5...1.5)
+        Slider(value: $rate, in: 0.1...1.9)
             .onChange(of: rate, perform: { value in
                 masterPlayer?.rate = value
                 print(rate)
@@ -60,12 +80,7 @@ struct ContentView: View {
         
         HStack{
             Button(action: {
-                do {
-                    player1 = try AVAudioPlayer(contentsOf: recorder!.url)
-                    player1?.prepareToPlay()
-                } catch {
-                    print("Can't play: \(error)")
-                }
+                setupPlayer1()
             }, label: {
                 Text("Store 1")
             })
@@ -79,12 +94,7 @@ struct ContentView: View {
         
         HStack{
             Button(action: {
-                do {
-                    player2 = try AVAudioPlayer(contentsOf: recorder!.url)
-                    player2?.prepareToPlay()
-                } catch {
-                    print("Can't play: \(error)")
-                }
+                setupPlayer2()
             }, label: {
                 Text("Store 2")
             })
@@ -95,7 +105,6 @@ struct ContentView: View {
                 Text("Play 2")
             })
         }
-        
     }
     
     func setup() {
@@ -122,16 +131,98 @@ struct ContentView: View {
         recorder?.prepareToRecord()
         
     }
-    
+
     func setupMasterPlayer() {
         masterPlayer = try! AVAudioPlayer(contentsOf: recorder!.url)
         masterPlayer?.enableRate = true
     }
-    
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    func setupPlayer1() {
+        do {
+            player1 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player1?.enableRate = true
+            player1?.rate = rate
+            player1?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
     }
+
+    func setupPlayer2() {
+        do {
+            player2 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player2?.enableRate = true
+            player2?.rate = rate
+            player2?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
+    }
+
+    func setupPlayer3() {
+        do {
+            player3 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player3?.enableRate = true
+            player3?.rate = rate
+            player3?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
+    }
+
+    func setupPlayer4() {
+        do {
+            player4 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player4?.enableRate = true
+            player4?.rate = rate
+            player4?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
+    }
+
+    func setupPlayer5() {
+        do {
+            player5 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player5?.enableRate = true
+            player5?.rate = rate
+            player5?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
+    }
+
+    func setupPlayer6() {
+        do {
+            player6 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player6?.enableRate = true
+            player6?.rate = rate
+            player6?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
+    }
+
+    func setupPlayer7() {
+        do {
+            player7 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player7?.enableRate = true
+            player7?.rate = rate
+            player7?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
+    }
+
+    func setupPlayer8() {
+        do {
+            player8 = try AVAudioPlayer(contentsOf: recorder!.url)
+            player8?.enableRate = true
+            player8?.rate = rate
+            player8?.prepareToPlay()
+        } catch {
+            print("Can't play: \(error)")
+        }
+    }
+
 }
